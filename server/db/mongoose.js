715ad227421ab05686df04ed2898a207ffb09e5e
Aurelien Callens <aurelien@infinitix.io>
2017-05-20 16:55:05 +0200

    Setup separate test & dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // tell Mongoose to use the javacript promises library
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
